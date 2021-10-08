da0bb73a07c142845719951cec3c7ae9228912f2
ralph <tmdrl1026@naver.com>
2021-10-08 10:16:06 +0900

    Fix MainActivity

--- app/src/main/java/com/example/simpledaggerhilt/ui/main/MainActivity.kt
+++ app/src/main/java/com/example/simpledaggerhilt/ui/main/MainActivity.kt
@@ -1,48 +1,36 @@
-package com.example.simpledaggerhilt
+package com.example.simpledaggerhilt.ui.main
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import androidx.activity.viewModels
-import androidx.lifecycle.Lifecycle
-import androidx.lifecycle.flowWithLifecycle
 import com.example.simpledaggerhilt.data.login.dto.LoginRequest
 import com.example.simpledaggerhilt.databinding.ActivityMainBinding
+import com.example.simpledaggerhilt.ui.folder.FolderActivity
+import com.example.simpledaggerhilt.ui.login.LoginActivity
 import com.example.simpledaggerhilt.viewmodels.LoginViewModel
 import dagger.hilt.android.AndroidEntryPoint
-import kotlinx.coroutines.flow.*
 import timber.log.Timber
 
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
-    private val viewModel: LoginViewModel by viewModels()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
 
-        setTestUser()
-        login()
-
+        goToLoginActivity()
     }
 
-    private fun login() {
-        binding.loginButton.setOnClickListener {
-            val id = binding.emailEditText.text.toString().trim()
-            val password = binding.passwordEditText.text.toString().trim()
-
-            val loginRequest = LoginRequest(id, password, "web")
-            viewModel.login(loginRequest)
-            Timber.d("MainActivity login()")
-        }
+    private fun goToLoginActivity(){
+        startActivity(Intent(this@MainActivity, LoginActivity::class.java))
+        finish()
     }
 
-    private fun setTestUser() {
-        binding.testButton.setOnClickListener {
-            binding.emailEditText.setText("textUser1")
-            binding.passwordEditText.setText("test123")
-        }
+    private fun goToFolderActivity(){
+        startActivity(Intent(this@MainActivity, FolderActivity::class.java))
+        finish()
     }
-
 }
