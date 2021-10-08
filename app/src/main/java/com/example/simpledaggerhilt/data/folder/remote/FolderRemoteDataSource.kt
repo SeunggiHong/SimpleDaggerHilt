5f75ed14177313f6a2f9e9e56b61a412a2e2333b
ralph <tmdrl1026@naver.com>
2021-10-08 10:18:52 +0900

    Add FolderRemoteDataSource

--- app/src/main/java/com/example/simpledaggerhilt/data/folder/remote/FolderRemoteDataSource.kt
+++ app/src/main/java/com/example/simpledaggerhilt/data/folder/remote/FolderRemoteDataSource.kt
@@ -1,2 +1,18 @@
 package com.example.simpledaggerhilt.data.folder.remote
 
+import com.example.simpledaggerhilt.api.FolderService
+import com.example.simpledaggerhilt.data.BaseDataSource
+import com.example.simpledaggerhilt.data.BaseResource
+import com.example.simpledaggerhilt.data.folder.dto.FolderResponse
+import retrofit2.Response
+import javax.inject.Inject
+
+class FolderRemoteDataSource @Inject constructor(
+    private val folderService: FolderService
+): BaseDataSource() {
+
+    suspend fun fetchFolderList() = getResult {
+        folderService.getFolder("list4client", "testUser")
+    }
+
+}
