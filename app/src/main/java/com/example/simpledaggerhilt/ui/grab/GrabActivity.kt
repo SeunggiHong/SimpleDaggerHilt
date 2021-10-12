5a2c918e8fc283c648adabd5b4fbe0c12a08cc8a
ralph <tmdrl1026@naver.com>
2021-10-12 10:51:28 +0900

    Add Grab Activity, viewmodel

--- app/src/main/java/com/example/simpledaggerhilt/ui/grab/GrabActivity.kt
+++ app/src/main/java/com/example/simpledaggerhilt/ui/grab/GrabActivity.kt
@@ -1,11 +1,52 @@
-package com.example.simpledaggerhilt.ui
+package com.example.simpledaggerhilt.ui.grab
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import androidx.activity.viewModels
+import androidx.lifecycle.Observer
+import com.example.simpledaggerhilt.R
+import com.example.simpledaggerhilt.data.BaseResource
+import com.example.simpledaggerhilt.databinding.ActivityGrabBinding
+import com.example.simpledaggerhilt.databinding.ActivityLoginBinding
+import dagger.hilt.android.AndroidEntryPoint
+import timber.log.Timber
 
+@AndroidEntryPoint
 class GrabActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityGrabBinding
+    private val viewModel: GrabViewModel by viewModels()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_grab)
+        binding = ActivityGrabBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        subscribeUi()
+    }
+
+    private fun subscribeUi() {
+        viewModel.allGrabList.observe(this, Observer { result ->
+            when(result.status) {
+                BaseResource.Status.SUCCESS -> {
+                    result.data?.memoList?.let {  list ->
+                        Timber.d("FolderActivity > subscribeUi > subscribeUi() > list : ${list}")
+                    }
+//                    binding.loading.visibility = View.GONE
+                }
+
+                BaseResource.Status.ERROR -> {
+                    result.message?.let {
+                        Timber.e(it)
+                    }
+//                    binding.loading.visibility = View.GONE
+                }
+
+                BaseResource.Status.LOADING -> {
+//                    binding.loading.visibility = View.VISIBLE
+                }
+            }
+
+        })
     }
 }
--- app/src/main/java/com/example/simpledaggerhilt/ui/grab/GrabViewModel.kt
+++ app/src/main/java/com/example/simpledaggerhilt/ui/grab/GrabViewModel.kt
@@ -1,2 +1,36 @@
 package com.example.simpledaggerhilt.ui.grab
 
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.simpledaggerhilt.data.BaseResource
+import com.example.simpledaggerhilt.data.folder.dto.FolderResponse
+import com.example.simpledaggerhilt.data.grab.GrabResponse
+import com.example.simpledaggerhilt.repository.GrabRepository
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.collect
+import kotlinx.coroutines.launch
+import timber.log.Timber
+import javax.inject.Inject
+
+@HiltViewModel
+class GrabViewModel @Inject constructor(
+    private val grabRepository: GrabRepository
+    ) : ViewModel() {
+    private val _allGrabList = MutableLiveData<BaseResource<GrabResponse>>()
+    val allGrabList: LiveData<BaseResource<GrabResponse>> = _allGrabList
+
+    init {
+        Timber.d("init GrabViewModel")
+        fetchAllGrabs()
+    }
+
+    private fun fetchAllGrabs() {
+        viewModelScope.launch {
+            grabRepository.fetchGrabAllList().collect { result ->
+                _allGrabList.value = result
+            }
+        }
+    }
+}
